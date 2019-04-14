4f54d47a3f723ca4e9e97ed3d305cdc603301d0a
Sosnus <sosnus15@gmail.com>
2019-04-15 01:13:52 +0200

    From Łukasz master and with Staszek methods, libs, objects

--- Projekt_samochod/Projekt_samochod.ino
+++ Projekt_samochod/Projekt_samochod.ino
@@ -1,60 +1,43 @@
-#include "Arduino.h"
+ /* TODO: Staszek */ // #include "Arduino.h"
 
 #include "Car.h"
-#include "Dbg.h"
+ /* TODO: Staszek */ // #include "Dbg.h"
 
-#define A_ENABLE 2 //PWM
-#define A_PHASE 46 //0 - przod, 1 - tyl DIGITAL
-
-#define B_ENABLE 3 //PWM
-#define B_PHASE 48 //0 - przod, 1 - tyl DIGITAL
+#define A_ENABLE    2 //PWM
+#define A_PHASE     46  //0 - przod, 1 - tyl DIGITAL
+  
+#define B_ENABLE    3 //PWM
+#define B_PHASE     48  //0 - przod, 1 - tyl DIGITAL
 
 Car car;
-Dbg dbg;
+ /* TODO: Staszek */ // Dbg dbg;
 
-void setup()
+void setup() 
 {
-  // // // // // // car.Init();
-  dbg.initialisation();
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+    /* TODO: Staszek */ // dbg.initialisation();
 }
 
-void loop()
+void loop() 
 {
-  dbg.checkCmd();
-
-
-  // if (Serial.available())
-  // {
-
-  //   /* constant-expression */
-  //   /* code */
-
-  //   // int inByte = Serial.read();
-  //   // Serial1.write(Serial.readBytes());
-  // }
-
-  // dbg.getY();
-  // delay(2000);
-
   // put your main code here, to run repeatedly:
-
-  /*
-  if(car.GetDistanceF() > 40)
+    /* TODO: Staszek */ // dbg.checkCmd(); 
+  if(car.getDistanceF() > 40)
     setEngines(150);
-  else if(car.GetDistanceF() < 40)
+  else if(car.getDistanceF() < 40)
     setEngines(0);
-    */
-
+  
   /*
   Serial.print("Distance: "); 
-  Serial.print(car.GetDistanceF()); 
+  Serial.print(car.getDistanceF()); 
   Serial.println(" cm");
-  delay(500); 
+  delay(500);
   */
 }
 
 void setEngines(int power)
 {
-  car.SetPowerLevel("left", power);
-  car.SetPowerLevel("right", power);
+    car.setPowerLevel("left", power);
+    car.setPowerLevel("right", power);
 }
