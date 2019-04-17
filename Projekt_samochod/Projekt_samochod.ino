d2e1b19670aaab336fc13c3f412bbf6ab3b0075f
Sosnus <sosnus15@gmail.com>
2019-04-17 17:30:48 +0200

    fixed car and added #if

--- Projekt_samochod/Projekt_samochod.ino
+++ Projekt_samochod/Projekt_samochod.ino
@@ -1,7 +1,11 @@
- /* TODO: Staszek */ // #include "Arduino.h"
+#define DBG_STA 1
 
-#include "Car.h"
- /* TODO: Staszek */ // #include "Dbg.h"
+ #include "Car.h"
+
+#if DBG_STA
+ #include "Arduino.h"
+ #include "Dbg.h"
+#endif
 
 #define A_ENABLE    2 //PWM
 #define A_PHASE     46  //0 - przod, 1 - tyl DIGITAL
@@ -9,36 +13,42 @@
 #define B_ENABLE    3 //PWM
 #define B_PHASE     48  //0 - przod, 1 - tyl DIGITAL
 
-Car car;
- /* TODO: Staszek */ // Dbg dbg;
-
+ Car car;
+#if DBG_STA
+ Dbg dbg;
+#endif
 void setup() 
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-    /* TODO: Staszek */ // dbg.initialisation();
+#if DBG_STA
+    dbg.initialisation();
+#endif
 }
 
 void loop() 
 {
-     /* TODO: Staszek */ // dbg.checkCmd(); 
   // put your main code here, to run repeatedly:
-  
+#if DBG_STA
+dbg.checkCmd(); 
+#endif
+
+
+/*
   if(car.getDistanceF() > 40)
     setEngines(150);
   else if(car.getDistanceF() < 40)
     setEngines(0);
-  
-  /*
+  */
   Serial.print("Distance: "); 
   Serial.print(car.getDistanceF()); 
   Serial.println(" cm");
   delay(500);
-  */
 }
-
+/*
 void setEngines(int power)
 {
     car.setPowerLevel("left", power);
     car.setPowerLevel("right", power);
 }
+*/
